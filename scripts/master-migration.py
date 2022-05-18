5350dc61a0ba555146a8224c24b29b526c0b4da3
elsonlim-11 <46041425+elsonlim-11@users.noreply.github.com>
2022-05-18 16:37:18 -0400

    Created first time migration script to convert existing Excel into a system friendly csv

--- scripts/master-migration.py
+++ scripts/master-migration.py
@@ -1,16 +1,21 @@
 import os
 import pandas as pd
-import polars as pl
-from pprint import pprint
-
-# import openpyxl
-# import pyarrow
 
 os.chdir(r'../')
 
 # Master is taken from the Jan - April extract
 pandasDF = pd.read_excel('data/raw/master.xlsx', sheet_name='User Data')
-masterDF = pl.DataFrame(pandasDF)
 
-print(masterDF.head())
-# pandasDF.head()
+renamedDF = pandasDF.rename(columns={
+    'E-mail': 'email',
+    'Name': 'name',
+    'Team': 'team',
+    'Title': 'title',
+    'Time saved (over the period)': 'timeSaved',
+    'Number of clicks (over the period)': 'numberOfClicks',
+    'mm/dd/yy': 'reportingDate',
+    'Region': 'region',
+    'City': 'city'
+})
+
+renamedDF.to_csv('data/processed/usage.csv', index=False)
